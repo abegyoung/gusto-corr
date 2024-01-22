017fb0ea0f33ec51e63a45dfd87a86d682483aff
Abram Young <agyoung@email.arizona.edu>
2024-01-21 21:16:56 -0700

    writeDB.py modded.  Depending on db.csv, hardcoded column indicies are bad. (level not always present)

--- writeCSVDB.py
+++ writeCSVDB.py
@@ -15,22 +15,23 @@ for row in c:
       #      name    time    scanID   volt
       name = row[0]
       time_cap = datetime.fromtimestamp(int(row[1])/1000000000).strftime("%Y-%m-%dT%H:%M:%SZ")
-      scanID = int(row[7])
-      volt = float(row[11])
+      scanID = int(row[6])
+      volt = float(row[10])
 
       json_body = [
-      {
-             "measurement": "{}".format(name),
-             "tags": {
-                 "scanID": "{}".format(scanID)
-             },
-             "time": "{}".format(time_cap),
-             "fields": {
-                 "volt": float(volt)
-             }
+         {
+            "measurement": "{}".format(name),
+            "tags": {
+               "scanID": "{}".format(scanID)
+            },
+            "time": "{}".format(time_cap),
+            "fields": {
+               "volt": float(volt)
+            }
          }
       ]
 
       print(json_body)
       client = InfluxDBClient('localhost', 8086, '', '', 'gustoDBlp')
       client.write_points(json_body)
+
