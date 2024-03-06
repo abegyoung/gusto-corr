5d55e8b19b6303236bbf399a1b82c92072090a4d
Abram Young <agyoung@email.arizona.edu>
2024-03-06 11:52:20 -0700

    convert udp ra,dec to l,b

---
+++ convert.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+import numpy
+from astropy import units as u
+from astropy.coordinates import SkyCoord
+
+table=numpy.genfromtxt("udp.txt", usecols=(-2,-1))
+
+for line in table[:6700000:100]:
+  c = SkyCoord(ra=line[0]*u.degree, dec=line[1]*u.degree, frame='icrs')
+  print(f'{c.galactic.l.value} {c.galactic.b.value}')
+
