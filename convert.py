85fd5eb7f283450d4fd0ab63eecdbe94a429de64
Abram Young <agyoung@email.arizona.edu>
2024-03-06 11:57:22 -0700

    update columns

--- convert.py
+++ convert.py
@@ -3,7 +3,7 @@ import numpy
 from astropy import units as u
 from astropy.coordinates import SkyCoord
 
-table=numpy.genfromtxt("udp.txt", usecols=(-2,-1))
+table=numpy.genfromtxt("udp.txt", usecols=(-3,-2))
 
 for line in table[:6700000:100]:
   c = SkyCoord(ra=line[0]*u.degree, dec=line[1]*u.degree, frame='icrs')
