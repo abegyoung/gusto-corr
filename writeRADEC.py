ec1876bb5279b8a5e9ab377f30925eb96114c84b
Abram Young <young@physics.arizona.edu>
2024-08-08 17:31:46 -0700

    Fixed UTC offset in fromtimestamp. Added lat, long, altitude and AZEL

--- writeRADEC.py
+++ writeRADEC.py
@@ -2,18 +2,29 @@ import sys
 import numpy
 from datetime import datetime
 from influxdb import InfluxDBClient
-
+from astropy.coordinates import EarthLocation, AltAz, SkyCoord
+from astropy.time import Time
+import astropy.units as u
 
 # Script to get UDP pointing data into an Influx Database from GUSTO series_*_*.tar.gz files
 
-table=numpy.genfromtxt(str(sys.argv[1]), usecols=(0,4,5,6))
+table=numpy.genfromtxt(str(sys.argv[1]), usecols=(0,1,2,3,4,5,6))
 for line in table: 
     #      name    time    scanID   volt
     name = "udpPointing"
-    time_cap = datetime.fromtimestamp(float(line[0])).strftime("%Y-%m-%dT%H:%M:%S.%fZ")
-    ra     = float(line[1])
-    dec    = float(line[2])
-    scanID =   int(line[3])
+    time_cap = datetime.utcfromtimestamp(float(line[0])).strftime("%Y-%m-%dT%H:%M:%S.%fZ")
+    alt    = float(line[1])
+    lat    = float(line[2])
+    lon    = float(line[3])
+    ra     = float(line[4])
+    dec    = float(line[5])
+    scanID =   int(line[6])
+
+    location = EarthLocation(lat=lat*u.deg, lon=lon*u.deg, height=alt*u.m)
+    observation_time = Time(time_cap)
+    sky_coord = SkyCoord(ra=ra*u.deg, dec=dec*u.deg, frame='icrs')
+    altaz_frame = AltAz(obstime=observation_time, location=location)
+    altaz_coord = sky_coord.transform_to(altaz_frame)
 
     json_body = [
         {
@@ -23,8 +34,13 @@ for line in table:
             },
         "time": "{}".format(time_cap),
             "fields": {
-                "RA": float(ra),
-                "DEC": float(dec)
+                "ALT": float(alt),
+                "LAT": float(lat),
+                "LON": float(lon),
+                "RA":  float(ra),
+                "DEC": float(dec),
+                "AZ":  float(altaz_coord.az.deg),
+                "EL":  float(altaz_coord.alt.deg)
             }
         }
     ]
