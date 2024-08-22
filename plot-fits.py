0c8075c666abaa5dbdb1efb7d7432c02c36233ae
Abram Young <young@physics.arizona.edu>
2024-08-22 08:21:52 -0700

    select one mixer to plot from extension table

--- plot-fits.py
+++ plot-fits.py
@@ -5,15 +5,19 @@ from astropy.io import fits
 import gc
 
 # Function to extract column data from the binary table extension
-def get_column_data(fits_file, column_names):
+def get_column_data(fits_file, column_names, mixer):
+    x_column_name, y_column_name = column_names
     try:
         with fits.open(fits_file, memmap=True) as hdul:
             # Assuming the binary table extension is named 'DATA_TABLE'
             data = hdul['DATA_TABLE'].data
-            column_data = [data[column_name] for column_name in column_names]
-            if ((column_data[1][0]<200) or (column_data[1][0]>400)):
-                print(fits_file)
-            return column_data
+            # Applf filter condition for dev
+            mask = data['MIXER'] == mixer
+            #column_data = [data[column_name] for column_name in column_names]
+            x_data = data[x_column_name][mask]
+            y_data = data[y_column_name][mask]
+            #return column_data
+            return x_data, y_data
     except Exception as e:
         print(f"Error reading {fits_file}: {e}")
         return [None, None]
@@ -23,15 +27,16 @@ directory = "./"
 # Glob pattern to find all FITS files
 fits_files = glob.glob(f"{directory}/*.fits")
 
-# The column names you want to plot (X and Y axes)
-x_column_name = 'scanID'
-y_column_name = 'THOT'
+# The column names and mixer you want to plot (X and Y axes)
+x_column_name = 'UNIXTIME'
+y_column_name = 'PSat'
+mixer=8
 
 # Extract column data from all FITS files
 all_x_data = []
 all_y_data = []
 for fits_file in fits_files:
-    x_data, y_data = get_column_data(fits_file, [x_column_name, y_column_name])
+    x_data, y_data = get_column_data(fits_file, [x_column_name, y_column_name], mixer)
     if x_data is not None and y_data is not None:
         all_x_data.extend(x_data)
         all_y_data.extend(y_data)
