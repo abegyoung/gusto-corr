ee3656569983f2ef25ca5eae79d5ee58aed41940
Abram Young <young@physics.arizona.edu>
2025-01-24 10:31:35 -0700

    edited tar_and_send

--- tar_and_send.sh
+++ tar_and_send.sh
@@ -1,16 +1,16 @@
 #!/bin/bash
 
 # Define variables
-LOCAL_DIR="./"
+LOCAL_DIR="/home/young/Projects/GUSTO/Data/level0.8"
 REMOTE_USER="obs"
 REMOTE_HOST="soral.as.arizona.edu"
-REMOTE_DIR="/storage/archive/GUSTO/level0.7/Oct16_B1"
+REMOTE_DIR="/storage/archive/GUSTO/level0.7/Oct16_B2"
 START_NUM=$1 # Replace with M (e.g., 00001)
 END_NUM=$2 # Replace with N (e.g., 99999)
-ARCHIVE_NAME="ACS5_$START_NUM-$END_NUM.tar.gz"
+ARCHIVE_NAME="ACS3_$START_NUM-$END_NUM.tar.gz"
 
 # Use tar to create an archive and send it via ssh to the remote machine
-tar -czf - -C "$LOCAL_DIR" $(ls "$LOCAL_DIR" | grep -E '.*[0-9]{5}.*' | awk -v start="$START_NUM" -v end="$END_NUM" '
+tar -czf - -C "$LOCAL_DIR" $(ls "$LOCAL_DIR"/ACS3_* | grep -E '.*[0-9]{5}.*' | awk -v start="$START_NUM" -v end="$END_NUM" '
 {
   # Extract the 5-digit number from the filename
   match($0, /[0-9]{5}/, arr);
